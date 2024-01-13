6942376ad95678506fed43947f183e87f5590519
BorisAtias <107293483+BorisAtias@users.noreply.github.com>
2024-01-13 10:33:46 +0100

    Delete Iterators and Generators - Exercise directory

--- Iterators and Generators - Exercise/09. Possible permutations.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-def possible_permutations(nums):
-    for perm in permutations(nums):
-        yield list(perm)
-
-
-
-[print(n) for n in possible_permutations([1, 2, 3])]
