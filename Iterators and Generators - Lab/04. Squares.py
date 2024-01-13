c36b625ed7d75f8d170f9c5d4ebdde868754dd1b
BorisAtias <107293483+BorisAtias@users.noreply.github.com>
2024-01-13 10:33:37 +0100

    Delete Iterators and Generators - Lab directory

--- Iterators and Generators - Lab/04. Squares.py
+++
@@ -1,6 +0,0 @@
-def squares(n):
-    for i in range(1, n + 1):
-        yield i ** 2
-
-
-print(list(squares(5)))
--- Iterators and Generators - Lab/06. Reverse string.py
+++
@@ -1,6 +0,0 @@
-def reverse_text(my_string):
-    for char in reversed(my_string):
-        yield char
-
-for char in reverse_text("step"):
-    print(char, end='')
